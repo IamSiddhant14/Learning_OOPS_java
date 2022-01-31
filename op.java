f9f1a98757ec9770701180c14c137e79080e41e2
Siddhant <siddhantsharma669@gmail.com>
2022-01-31 11:34:39 +0530

    Diff bw running and compiling

--- op.java
+++ op.java
@@ -3,6 +3,10 @@
 //An IDE consist of both editor and compiler
 //An jdk(Java development kit) is been used to to compile and run a java program
 
+//If the file name and the class name are same
+
+//For compiling - javac <-- filename.java --/>
+
 import java.util.*; //This will have all the opertions like that of print ,etc, etc
 import java.io.*;//This is rquired for input output operation
 
@@ -46,6 +50,11 @@ public class op{ //When we have a public class then the class name should be sam
 //https://zoom.us/rec/play/31N2bK36Ljg4ycpYumbwqaJG9wQ0dI_Mo_sWmULLwEHSDQZ8xgPZobGaAvSwoiGjYo2diNgV3J4kbD2U.EH9rs_v29VTBj3Q3?continueMode=true&_x_zm_rtaid=mCLAPnYxTfa8MaX6DfO36w.1643570106035.1367397f1c5250b3cd6668307d9a2b59&_x_zm_rhtaid=422
 
 //while compiling the name could be diffrent but while running the class name should be same as the class actuale name or it will result in an run time error
+
+//If the file name and the class name are DIFFRENT
+
+//For compiling - javac <-- filename.java --/>
+//For running - java <-- class Name --/>
 
 // class o{//If we dont write "public" in front of class name then we could create name of class as per our needs and not same as the file name as before
 
