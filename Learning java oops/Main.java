ce57da3b1d5b6c8c721b0239a403b26789fd3859
Siddhant <siddhantsharma669@gmail.com>
2022-03-14 17:40:06 +0530

    Added

--- Learning java oops/Main.java
+++ Learning java oops/Main.java
@@ -4,6 +4,12 @@
 //To compile  and run it in one go without creating any  .class file we will do :
 // java Main.java   (Here filename with extension is required)
 
+//If we wish to create our own data type we can do it using classes
+
+//A class consist of a data member and member functions
+
+
+
 
 
 
@@ -21,17 +27,69 @@ public class Main{
 
     public static void main( String[] args){// Here this " String[] args "represent that it is an array of string named as args 
         
-        // 1.> Here this method "main" is also public in nature as we dont require to create an object of the class "Main" in order to invoke the "main" method thats why it is been made as public in nature
+        // 1.> Here this method "main" is also static in nature as we dont require to create an object of the class "Main" in order to invoke the "main" method thats why it is been made as public in nature
+
+        // 1.1> Main obj = new Main();
+        // obj.main(); we dont like to do it like this way thats why
         
         // 2.> Here the name of this method will always be "main" (Syantx)
 
         // 3.> Here void is the return type of the function
 
-        System.out.println(args[0]); // To pass argument in to the main function we could write it like : 
+        //4.> Program is always going to start from the main function
+
+        //5.> Here public signifience that this method could be accessed from any other package any other class or any file as well
+
+        //** */ System.out.println(args[0]); // To pass argument in to the main function we could write it like : 
 
         //First compile it : javac Main.java
         //Run it with arguments : java Main 3 "Siddhant"
         //Here this will go in the form of an array of type string"[ "3" , "Siddhant "]"
 
-    }
+
+        //Notes : Any data type which could not be broken into other data types are called premitive data type like( stavk mai he refrence variable banta hai) :
+
+        // int rollos = 64;
+        // char letter =  'r'; 
+        // float marks = 98.67f;
+        // double largeDecimalNumbers = 4567654.4567;
+        // long largeInteger = 34567834567876543L;
+        // boolean check = false;
+
+        //Wrapper classes : Stack mai reference variable banta hai or uska object heap mai banta hai jis ko refernce variable point karta hai
+
+        Integer rno = 13;
+        Integer n = new Integer(13);
+
+
+
+
+// FROM LEARNING INTELLIJ
+
+//         System.out.println(args[0]);
+//         System.out.print("Please entre some input :");
+//         Scanner scn = new Scanner(System.in);//Keyboard
+//         String s = scn.next();
+// //      System.out.println("Hello World : "+ args[0]); To run this:             java   Main.java "Siddhant"
+//         System.out.println("Hello World : "+ s);//Command line, display
+
+
+class Student {
+    int roll = 3 ;
+    int grade;
+    String name;
 }
+
+Student s = new Student();// Allocating a variable at run time and returns a refrence to it
+System.out.println(s.roll);     
+
+}
+
+//Final keyword help us in preventing our content getting modified, remember final variables need to be intialized at the time of creating it, final keyword is only applicable to the premitive data types only as in case of non premative data type we could not reassign it
+final int INCREASE = 2; 
+//  INCREASE = 3;//This will result in an error
+
+
+}
+
+
